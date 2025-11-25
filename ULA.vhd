d78642bff7c1a4ca1ac7e87ed8776d504959ff0c
Wallacy Alvarenga <wallacyalvarenga@gmail.com>
2025-11-25 20:32:49 -0300

    ULA corrigido

--- ULA.vhd
+++ ULA.vhd
@@ -1,4 +1,4 @@
---ULA incompleta, faltando várias operações.
+--ULA completo
 --by Daniel Uesler de Brito.
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -10,8 +10,10 @@ entity ULA is
 	port(
     	i_A  		: in  std_logic_vector(31 downto 0); --std_logic_vector é similar a bit_vector
         i_B  		: in  std_logic_vector(31 downto 0);
+        i_CODE      : IN  std_logic_vector(6 DOWNTO 0); -- 7 bits operacao
         i_F3 		: in  std_logic_vector(2 downto 0);
         i_INST30	: in std_logic;
+        i_ALUOP     : in  std_logic_vector(2 downto 0); -- 3 bits operacao
         o_ZERO		: in std_logic;
         o_ULA  		: out std_logic_vector(31 downto 0)
     );
@@ -22,37 +24,20 @@ end ULA;
 architecture a1 of ULA is
 
 begin
-	process(i_A, i_B, i_F3, i_INST30)
+	process(i_A, i_B,i_ALUOP)
     begin
-    	if (i_F3 = "000") then
-            if(i_INST30 = '0') then
-        		o_ULA <= i_A + i_B;
-            else
-            	o_ULA <= i_A - i_B;
-            end if;
-        elsif (i_F3 = "100") then
-        	o_ULA <= i_A xor i_B;
+    	if (i_ALUOP = "000") then
+            o_ULA <= i_A + i_B; -- ADD
+        elsif (i_ALUOP = "001") then
+            o_ULA <= i_A - i_B; -- SUB
+        elsif (i_ALUOP = "010") then
+            o_ULA <= i_A and i_B; -- AND
+        elsif (i_ALUOP = "011") then
+            o_ULA <= i_A or i_B; -- OR
+        elsif (i_ALUOP = "100") then
+            o_ULA <= i_A xor i_B; -- XOR
+        else
+            o_ULA <= i_A + i_B; -- padrao ADD
         end if;
     end process;
-    
-    --process(i_A, i_B, i_F3)
-    --begin
-    --	case i_F3 is
-    --    	when "000" => 
-    --        	if(i_INST30 = '0') then
-    --    			o_ULA <= i_A + i_B;
-    --       	else
-    --        		o_ULA <= i_A - i_B;
-    --        	end if;
-    --       when "100" => o_ULA <= i_A xor i_B;
-    --       
-    --       when OTHERS => o_ULA <= (others => '0');    
-    --	end case;
-    --end process;
-    
-    --o_ULA <= 	i_A+i_B 	when i_F3 = "000" and i_INST30 = '0' else
-    --			i_A-i_B 	when i_F3 = "000" and i_INST30 = '1' else
-    --            i_A xor i_B when i_F3 = "100" else (others => '0'); 
-    
-
 end a1;
